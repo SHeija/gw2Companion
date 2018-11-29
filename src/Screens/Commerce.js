b0e93fcda76dde1a2b4da3b7b2599bd8929c686f
SHeija <sirkku.heija@myy.haaga-helia.fi>
2018-11-29 21:13:34 +0200

    Added wallet, fixed commerce page async-flow

--- src/Screens/Commerce.js
+++ src/Screens/Commerce.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { View, Text, Alert, ScrollView, AsyncStorage } from 'react-native';
 import { List, ListItem, } from 'react-native-elements';
-
-import { getData } from '../Data/ApiHelper';
-import { getInfo } from '../Data/ApiHelper';
-
-
+import { getData, getInfo } from '../Data/ApiHelper';
 
 export default class Commerce extends React.Component {
     constructor(props){
         super(props);
-        this.state = { apiKey:"", delivery: {}, saleHistory:{}, loading:true };
+        this.state = { apiKey:"", delivery: {}, wallet:{}, loading:true, invalidKey:false};
     }
 
     componentDidMount(){
-        this.loadSettings();
-        this.updateData();
+        this.loadSettings()
+        .then(() =>{
+            this.updateData()
+        });
 
         this.props.navigation.addListener("didFocus", () => {
-            this.loadSettings();
-            this.updateData();
+            this.loadSettings()
+            .then(() =>{
+                this.updateData()
+            });
         });
 
     }
@@ -39,52 +39,99 @@ export default class Commerce extends React.Component {
     }
 
     updateData = async () => {
+
         //0642AC9B-83BC-424B-ACAE-1CF086593ACF40519FC7-C3AB-47BE-85D5-3113C0F99ADF
+        //check if the key is valid
         const apiKey = this.state.apiKey;
-        const url =  'https://api.guildwars2.com/v2/commerce/delivery?access_token='+apiKey;
-        const delivery = await getData(url);
-        
-        const items = await getInfo(delivery.items, 'https://api.guildwars2.com/v2/items?ids=');
-        delivery["items"]= items;
-        this.setState({
-            delivery: {...this.state.delivery, ...delivery},
-        }, () => {
+        const apiKeyCheckUrl = 'https://api.guildwars2.com/v2/tokeninfo?access_token='+apiKey;
+        let keyIsValid;
+        const apiKeyData = await getData(apiKeyCheckUrl);
+        if (apiKeyData.hasOwnProperty('id')){
+            keyIsValid=true;
+        }else{
+            keyIsValid=false;
+        } 
+
+        //attempt only if key is valid
+        if (keyIsValid){
+            
+            const delivery = await this.getDelivery(apiKey);
+            const wallet = await this.getWallet(apiKey);
+            
             this.setState({
-                loading:false
+                delivery: {...this.state.delivery, ...delivery},
+                wallet: wallet,
+                invalidKey: false,
+                loading: false
             });
-        });
+        }else{
+            this.setState({
+                invalidKey:true,
+                loading: false
+            })
+        }
+        
         
     }
 
+    getDelivery = async (apiKey) => {
+        const deliveryUrl =  'https://api.guildwars2.com/v2/commerce/delivery?access_token='+apiKey;
+        const delivery = await getData(deliveryUrl);
+        const itemUrl = 'https://api.guildwars2.com/v2/items?ids=';
+        const deliveryItems = await getInfo(delivery.items, itemUrl);
+        delivery["items"]= deliveryItems;
+        return delivery;
+    }
+
+    getWallet = async (apiKey) => {
+        const walletUrl = 'https://api.guildwars2.com/v2/account/wallet?access_token='+apiKey;
+        const wallet = await getData(walletUrl);
+        const currencyUrl = 'https://api.guildwars2.com/v2/currencies?ids=';
+        const walletNames = await getInfo(wallet, currencyUrl);
+        for (let i = 0; i<Object.keys(wallet).length;i++){
+            for (let j = 0; j<Object.keys(walletNames).length;j++){
+                if (wallet[i].id == walletNames[j].id){
+                    wallet[i]["name"] = walletNames[j].name;
+                    //wallet[i]["icon"] = walletNames[j].icon;
+                }
+            }
+        }
+        return wallet;
+    }
+
     render(){
-        
+
         //Looks stupid, works
         const gold = parseInt(this.state.delivery.coins/100/100);
         const silver = parseInt((this.state.delivery.coins/100/100-gold)*100);
         const copper = parseInt(((this.state.delivery.coins/100/100-gold)*100-silver)*100);
         
         if (this.state.apiKey == null || this.state.apiKey == ""){
-                return(
-                    <View><Text>No API key found</Text></View>
-                );
+            return(
+                <View><Text>No API key found</Text></View>
+            );
         }else if (this.state.loading){
             return(
-                    <View><Text>Loading!</Text></View>
+                <View><Text>Loading!</Text></View>
+            );
+        }else if(this.state.invalidKey){
+            return(
+                <View><Text>Invalid API key</Text></View>
             );
         }else{
-                return(
-                    <View>
-                        <ScrollView>
-                            <Text>Waiting for Pickup</Text>
-                            <View>
-                                <Text>Coins:</Text>
-                                <Text> {gold} gold, {silver} silver, {copper} copper</Text>
-                            </View>
-                            <View>
-                                <Text>Items:</Text>
-                                <List>
-                                    {
-                                        this.state.delivery.items.map((item) => (
+            return(
+                <View>
+                    <ScrollView>
+                        <Text>Waiting for Pickup</Text>
+                        <View>
+                            <Text>Coins:</Text>
+                            <Text> {gold} gold, {silver} silver, {copper} copper</Text>
+                        </View>
+                        <View>
+                            <Text>Items:</Text>
+                            <List>
+                                {
+                                    this.state.delivery.items.map((item) => (
                                         <ListItem
                                             key={item.id}
                                             title={item.name}
@@ -93,13 +140,28 @@ export default class Commerce extends React.Component {
                                             subtitleNumberOfLines = {5}
                                             hideChevron
                                         />
-                                        ))
-                                    }
-                                </List>
-                            </View>
-                            
-                        </ScrollView>
-                    </View>
+                                    ))
+                                }
+                            </List>
+                        </View>
+                        <Text>Wallet</Text>
+                        <List>
+                        {
+                                    this.state.wallet.map((item) => (
+                                        <ListItem
+                                            key={item.id}
+                                            title={item.name}
+                                            subtitle={item.value}
+                                            leftAvatar={{ source: { uri: item.icon } }}
+                                            subtitleNumberOfLines = {5}
+                                            hideChevron
+                                        />
+                                    ))
+                                }
+                        </List>
+                        
+                    </ScrollView>
+                </View>
             );
         }
         
